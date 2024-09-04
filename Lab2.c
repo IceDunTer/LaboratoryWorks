ef688b81772713ea33353ba091327870c8a9154f
IceDunTer <130907228+IceDunTer@users.noreply.github.com>
2024-09-04 16:52:00 +0300

    Задание 3

---
+++ Lab2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    puts(" _   _     __   __    __    _ ");
+    puts("| | |_    |  | |_    |  |  |_ ");
+    puts(" /  | |   |  |   |   |  |  | |");
+    puts("/_  |_|   |__| __|   |__|  |_|");
+
+    getchar();
+
+}
