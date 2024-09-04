ff4a433af5e98b5fd5d1784285abfd21b2caaeac
IceDunTer <130907228+IceDunTer@users.noreply.github.com>
2024-09-04 16:33:16 +0300

    Задание 2

---
+++ Lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    puts("* * * * * * * * * * * * * * * * * * * * * *");
+    puts("*                                         *");
+    puts("* тема: Разработка консольного приложения *");
+    puts("*          Выполнил Боев К.С.             *");
+    puts("*                                         *");
+    puts("* * * * * * * * * * * * * * * * * * * * * *");
+    getchar();
+
+}
