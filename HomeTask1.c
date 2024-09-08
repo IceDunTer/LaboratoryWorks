f876687a9799eaac80fef1ce8a1161936da21fa1
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-09-08 18:48:58 +0300

    Add files via upload

---
+++ HomeTask1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+void main() {
+    puts("          _");
+    puts("         / \\");
+    puts("        /   \\ ");
+    puts("       /  _  \\ ");
+    puts("      /  / \\  \\  ");
+    puts("     /   \\_/   \\     ");
+    puts("    /___________\\     ");
+    puts("   /|    ___    |\\   ");
+    puts("    |   |_|_|   | ");
+    puts("    |   |___|   |  ");
+    puts("    |    ___    |  ");
+    puts("    |   |  ,| o |  ");
+    puts("    |___|   |___|");
+    puts("    |   \\___/  |  ");
+    getchar();
+}
