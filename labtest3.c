39e06aefe91ceb3c6e45bf7d1dfb1e858ff994ba
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-09-18 15:41:51 +0300

    labtest3.c

---
+++ labtest3.c
@@ -0,0 +1,16 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	float a, b, a1, b1;
+
+	puts("Введите число А:\n");
+	scanf("%f", &a);
+	puts("Введите число B:\n");
+	scanf("%f", &b);
+	printf("|\t\ta * b\t\t|\t\ta + b\t\t|\t\ta - b\t\t|\n");
+	printf("|%f * %f|%f + %f|%f - %f|\n", a, b, a, b, a, b);
+	printf("|\t\t%f\t|\t\t %f\t|\t\t%f\t|\n", a*b, a+b, a-b);
+}
