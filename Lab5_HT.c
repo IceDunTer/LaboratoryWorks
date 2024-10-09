848cdf2b0ab5ffaf9d7a1d579abc082644146cba
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-10-09 11:27:05 +0300

    Lab5_HT.c

---
+++ Lab5_HT.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#define _USE_MATH_DEFINES
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	double A, B, C, D, x, y, z, v;
+
+	x = 3.74/100;
+	y = -0.825;
+	z = 0.16*100;
+
+	A = 1 + pow(sin(x + y), 2);
+	B = fabs(x - (2 * y / 1 + x * x * y * y));
+	C = pow(x, fabs(y));
+	D = pow(cos(atan(1 / z)), 2);
+
+	v = A / B * C + D;
+
+	printf("%lg", v);
+
+}
