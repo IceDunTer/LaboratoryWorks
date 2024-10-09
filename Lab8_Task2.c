68c75c62bee308d74da6ca53dccb4aea0abe34d3
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-10-09 10:38:53 +0300

    Lab8_Task2.c

--- Lab8_Task2.c
+++ Lab8_Task2.c
@@ -5,38 +5,38 @@
 #define Pi 3.14159265358979323846
 
 // y = x^2 - cos^2 пx [1;3]
-// y = 2
+// y = 3
 int main() {
 	setlocale(LC_ALL, "RUS");
 	double x;
 	float y;
 	int tab;
 	puts("Введите Х в интервале [1;3]:\n");
-	scanf_s("%lg", &x);
+	scanf("%lg", &x);
 
 	y = pow(x, 2) - pow(cos(Pi * x), 2);
 
 	puts("Введите шаг табуляции:\n");
-	scanf_s("%i", &tab);
+	scanf("%i", &tab);
 
 	printf("| ");
 	printf("(x)");
-	for (int i = 0; i <= tab; i++) {
+	for (int i = 0; i < tab; i++) {
 		printf("\t");
 	}
 	printf("| f(x)");
-	for (int i = 0; i <= tab; i++) {
+	for (int i = 0; i < tab; i++) {
 		printf("\t");
 	}
 	printf("|\n");
 
 	printf("| ");
 	printf("(%.2lg)", x);
-	for (int i = 0; i <= tab; i++) {
+	for (int i = 0; i < tab; i++) {
 		printf("\t");
 	}
 	printf("| %.2f", y);
-	for (int i = 0; i <= tab; i++) {
+	for (int i = 0; i < tab; i++) {
 		printf("\t");
 	}
 	printf("|");
