535f95088f174c2a99974c04810d02507a4ca242
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-10-09 10:08:31 +0300

    Lab8_Task2.c

---
+++ Lab8_Task2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#define Pi 3.14159265358979323846
+
+// y = x^2 - cos^2 пx [1;3]
+// y = 2
+int main() {
+	setlocale(LC_ALL, "RUS");
+	double x;
+	float y;
+	int tab;
+	puts("Введите Х в интервале [1;3]:\n");
+	scanf_s("%lg", &x);
+
+	y = pow(x, 2) - pow(cos(Pi * x), 2);
+
+	puts("Введите шаг табуляции:\n");
+	scanf_s("%i", &tab);
+
+	for (int i = 0; i <= tab; i++) {
+		printf("\t");
+	}
+	
+}
