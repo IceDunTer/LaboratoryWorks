5e359916ee34886a5f6c4f1f9e84167d2975394c
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-12-04 10:38:19 +0300

    Lab15_1.c

---
+++ Lab15_1.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define m_row 100
+#define m_col 100
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int row, col;
+	double num;
+	puts("Введите количество столбцов:");
+	scanf_s("%i", &row);
+	puts("Введите длину строки:");
+	scanf_s("%i", &col);
+
+	double arr[m_row][m_col];
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			arr[i][j] = i + j / 10.;
+		}
+	}
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf("%.2f ", arr[i][j]);
+
+		}
+		printf("\n");
+	}
+}
