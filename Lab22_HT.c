f16a322353366746da709827426a2f6a6f63db00
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-12-25 23:29:10 +0300

    Lab22_HT.c

--- Lab22_HT.c
+++ Lab22_HT.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#define N 10
+
+const double M_PI = 3.14159265358979323846;
 
 typedef double (*math_func)(double);
 
@@ -11,16 +14,30 @@ double Y(double x) {
 
 double V(double x) {
     if (x > 1) {
-        return sqrt(tan(x * x - 1)) - 2 * x;
-    } else if (0 <= x && x <= 1) {
-        return cos(x);
-    } else {
-        return 0;
+        double arg = x * x - 1;
+        if (fabs(arg) < M_PI / 2) {
+            return sqrt(tan(arg));
+        }
+        else {
+            return -9999;
+        }
+    }
+    else if (0 <= x && x <= 1) {
+        return -2 * x;
+    }
+    else {
+        return exp(cos(x));
     }
 }
 
 double S(double x) {
-    return (x - 1) * (1 / (x + 1)) + pow((x - 1) / (x + 1), 3) / 3 + pow((x - 1) / (x + 1), 5) / 5 + pow((x - 1) / (x + 1), 7) / 7;
+    if (x != -1) {
+        double term = (x - 1) / (x + 1);
+        return term + 1 / 3 * pow(term, 3) + 1 / 5 * pow(term, 5) + 1 / 7 * pow(term, 7);
+    }
+    else {
+        return -9999;
+    }
 }
 
 void tab(math_func func, double start, double end, double step) {
@@ -31,7 +48,7 @@ void tab(math_func func, double start, double end, double step) {
     printf("\n");
 }
 
-double calc(math_func prtf, double *x, int n) {
+double calc(math_func prtf, double* x, int n) {
     double product = 1.0;
     int count = 0;
 
@@ -42,11 +59,10 @@ double calc(math_func prtf, double *x, int n) {
             count++;
         }
     }
-
     return (count > 0) ? product : 0;
 }
 
-void perform_operation(math_func func, double *x, int n) {
+void perform_operation(math_func func, double* x, int n) {
     double result = calc(func, x, n);
     printf("Произведение значений функции, для которых 0 < y < 3.2: %7.4f\n", result);
 }
@@ -84,59 +100,57 @@ int main() {
         scanf("%i", &func_choice);
 
         switch (func_choice) {
-            case 1:
-                selected_func = S;
-                break;
-            case 2:
-                selected_func = V;
-                break;
-            case 3:
-                selected_func = Y;
-                break;
-            default:
-                printf("Некорректный выбор функции.\n");
-                continue;
+        case 1:
+            selected_func = S;
+            break;
+        case 2:
+            selected_func = V;
+            break;
+        case 3:
+            selected_func = Y;
+            break;
+        default:
+            printf("Некорректный выбор функции.\n");
+            continue;
         }
 
         switch (choice) {
-            case 1: {
-                double x;
-                printf("Введите значение x: ");
-                scanf("%lg", &x);
-                printf("Значение функции при x = %.2lg: %.4lg\n", x, selected_func(x));
-                break;
-            }
-            case 2:
-                printf("Введите начало интервала: ");
-                scanf("%lg", &start);
-                printf("Введите конец интервала: ");
-                scanf("%lg", &end);
-                printf("Введите шаг табуляции: ");
-                scanf("%lg", &step);
-                tab(selected_func, start, end, step);
-                break;
-            case 3: {
-                printf("Введите количество значений x: ");
-                scanf("%i", &n);
-                double x[n];
-                printf("Введите значения x:\n");
-                for (int i = 0; i < n; i++) {
-                    scanf("%lg", &x[i]);
-                }
-                perform_operation(selected_func, x, n);
-                break;
-            }
-            case 4:
-                printf("Введите начало интервала: ");
-                scanf("%lg", &start);
-                printf("Введите конец интервала: ");
-                scanf("%lg", &end);
-                printf("Введите шаг табуляции: ");
-                scanf("%lg", &step);
-                plot(selected_func, start, end, step);
-                break;
-            default:
-                printf("Некорректный выбор действия.\n");
+        case 1: {
+            double x;
+            printf("Введите значение x: ");
+            scanf("%lg", &x);
+            printf("Значение функции при x = %.2lg: %.4lg\n", x, selected_func(x));
+            break;
         }
-    } while (!choice);
+        case 2:
+            printf("Введите начало интервала: ");
+            scanf("%lg", &start);
+            printf("Введите конец интервала: ");
+            scanf("%lg", &end);
+            printf("Введите шаг табуляции: ");
+            scanf("%lg", &step);
+            tab(selected_func, start, end, step);
+            break;
+        case 3: {
+            double x[N];
+            printf("Введите значения x:\n");
+            for (int i = 0; i < N; i++) {
+                scanf("%lg", &x[N]);
+            }
+            perform_operation(selected_func, x, N);
+            break;
+        }
+        case 4:
+            printf("Введите начало интервала: ");
+            scanf("%lg", &start);
+            printf("Введите конец интервала: ");
+            scanf("%lg", &end);
+            printf("Введите шаг табуляции: ");
+            scanf("%lg", &step);
+            plot(selected_func, start, end, step);
+            break;
+        default:
+            printf("Некорректный выбор действия.\n");
+        }
+    } while (choice != 0);
 }
