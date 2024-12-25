d7663cb674875fca9ce14841470568a64f63c44b
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-12-25 22:02:42 +0300

    Lab11_HT.c

--- Lab11_HT.c
+++ Lab11_HT.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
-#define N 10
 
 // Контрольный пример 1: [1, 3, 4, -2, 5, 6, 7, 8, 9, -24] - 8
 // Контрольный пример 2: [5, 11, 3, 4, 1, 2, -6, 2, -4, 2] - 26
 
+#include <stdio.h>
+
 int main() {
     setlocale(LC_ALL, "RUS");
-    double arr[N], a;
-    double summary = 0;
-    puts("Введите 10 чисел для заполнения массива:");
-    for (int i = 0; i < N; i++) {  
-        scanf_s("%lg", &a);
-        arr[i] = a;
-    }
-    for (int i = 0; i < N; i++) {
-        if (arr[i] >= 0) {
-            summary += arr[i];
-        }
-        else {
-            printf("Сумма всех элементов массива до последнего положительного элемента: %lg", summary);
-            break;
+    int n;
+    printf("Введите размер массива: ");
+    scanf("%d", &n);
+    int arr[n];
+    int lastPositiveIndex = -1;
+    
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] > 0) {
+            lastPositiveIndex = i;
         }
     }
 
+    if (lastPositiveIndex == -1) {
+        printf("В массиве нет положительных элементов.\n");
+        return 1;
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < lastPositiveIndex; i++) {
+        sum += arr[i];
+    }
+    printf("Сумма всех элементов, расположенных до последнего положительного элемента: %d\n", sum);
 }
