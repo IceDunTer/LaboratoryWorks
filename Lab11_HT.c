73a00c26919f7408a1704d50e8f2eed283022991
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2024-12-26 12:15:49 +0300

    Update Lab11_HT.c

--- Lab11_HT.c
+++ Lab11_HT.c
@@ -1,23 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
 
+#define n 10
+
 // Контрольный пример 1: [1, 3, 4, -2, 5, 6, 7, 8, 9, -24] - 8
 // Контрольный пример 2: [5, 11, 3, 4, 1, 2, -6, 2, -4, 2] - 26
 
-#include <stdio.h>
-
 int main() {
     setlocale(LC_ALL, "RUS");
-    int n;
-    printf("Введите размер массива: ");
-    scanf("%d", &n);
     int arr[n];
     int lastPositiveIndex = -1;
-    
+
     printf("Введите элементы массива:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%i", &arr[i]);
         if (arr[i] > 0) {
             lastPositiveIndex = i;
         }
@@ -27,10 +25,10 @@ int main() {
         printf("В массиве нет положительных элементов.\n");
         return 1;
     }
-    
+
     int sum = 0;
     for (int i = 0; i < lastPositiveIndex; i++) {
         sum += arr[i];
     }
-    printf("Сумма всех элементов, расположенных до последнего положительного элемента: %d\n", sum);
+    printf("Сумма всех элементов, расположенных до последнего положительного элемента: %i\n", sum);
 }
